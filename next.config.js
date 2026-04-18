4435cd09f1170e3f8698a1cf12dae8e76c768c2c
penguinpecker <penguinpecker1@gmail.com>
2026-04-18 14:29:53 +0530

    bump next to 15.1.11 (patches CVE-2025-66478) + silence optional-dep warnings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // MetaMask SDK and WalletConnect's logger both reference optional deps
+    // we never use in a browser build. Stub them out so they don't log.
+    config.externals = [
+      ...(config.externals || []),
+      "pino-pretty",
+      "@react-native-async-storage/async-storage",
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
